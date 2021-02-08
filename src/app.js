baeaf7edc6929c423eafc4aefdff59e7186929d6
Josue Tobar <tobarj53@gmail.com>
2021-02-07 23:31:23 -0600

    solucion problema de siseño
    solucion problema de siseño

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const passport = require('passport');
 const flash = require('connect-flash');
 const MySQLStore = require('express-mysql-session')(session);
 const bodyParser = require('body-parser');
+const corss = require('corss');                // *Acceder a la api desde cualquier lugar
 
 const { database, port } = require('./config');
 
@@ -19,6 +20,7 @@ require('./lib/passport');
 // Settings
 app.set('port', port);
 app.set('views', path.join(__dirname, 'views'));
+app.use(corss()); 
 app.engine('.hbs', exphbs({
   defaultLayout: 'main',
   layoutsDir: path.join(app.get('views'), 'layouts'),
@@ -30,8 +32,9 @@ app.set('view engine', '.hbs');
 
 // Middlewares
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
 
 app.use(session({
   secret: uuidv4(),
@@ -42,6 +45,18 @@ app.use(session({
 app.use(flash());
 app.use(passport.initialize());
 app.use(passport.session());
+
+const storage = multer.diskStorage({
+  destination: path.join(__dirname, 'public/uploads'),
+  filename: (req, file, cb) => {
+    cb(null, uuidv4() + path.extname(file.originalname).toLocaleLowerCase());
+  }
+});
+
+app.use(multer({
+  storage: storage,
+  dest: path.join(__dirname, 'public/uploads'),
+}).single('img'));
 
 // Global variables
 app.use((req, res, next) => {
