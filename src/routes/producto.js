bf77477e53556445e7a215d192ab60665a21a3c8
Josue Tobar <tobarj53@gmail.com>
2021-02-13 10:51:08 -0600

    Subir imagenes
    -- se configuro la subida de imagenes al server

--- src/routes/producto.js
+++ src/routes/producto.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../database');
+const Jimp = require('jimp');
+
 
 const { isLoggedIn } = require('../lib/auth');
 
@@ -18,4 +20,15 @@ router.get('/producto_edit', (req, res) => {
     res.render('producto/edit');
 });
 
+router.post('/producto_add', async (req, res) => {
+    console.log(req.file)
+    const filepath = Date.now()+req.file.filename.replace(/ /g, "")
+    const image = await Jimp.read(req.file.path)
+    image.resize(50, 50) 
+    .quality(90)
+    .writeAsync(`../public/TEST/${filepath}`)
+    res.json({
+        data: `![](http://localhost:4000/TEST/${filepath})`
+    })
+});
 module.exports = router;
