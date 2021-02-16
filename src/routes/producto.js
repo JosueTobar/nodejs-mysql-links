6e01f0706387790b8e4b118c44329909ac418b3c
Josue Tobar <tobarj53@gmail.com>
2021-02-16 00:01:52 -0600

    Insert producto mas historial
    Insert producto mas historial

--- src/routes/producto.js
+++ src/routes/producto.js
@@ -8,7 +8,7 @@ const { isLoggedIn } = require('../lib/auth');
 
 //Select menu 
 router.get('/producto_list', async (req, res) => {
-    const producto = await pool.query('SELECT * FROM inventario.menu;');
+    const producto = await pool.query('SELECT * FROM inventario.producto where IDUSUARIO = ?;',[req.user.idUSUARIO]);
     res.render('producto/list', { producto });
 });
 //
@@ -24,9 +24,15 @@ router.post('/producto_add', async (req, res) => {
     const filepath = req.file.filename;
     const image = await Jimp.read(req.file.path)
     image.resize(50, 50) 
-    await image.writeAsync(`uploads/${filepath}`)
-    res.json({
-        data: `![](http://localhost:4000/uploads/${filepath})`
-    })
+    await image.writeAsync(`uploads/${filepath}`);
+    console.log(req.user);
+    const { codigo, nombre, descripcion, tipounidad,minimo, pesoneto,pesobruto,cantidad } = req.body;
+    const result =  await pool.query('INSERT INTO `inventario`.`producto` (`CODIGO`, `NOMBRE`, `DESCRIPCION`, `TIPOUNIDAD`, `STOKMINIMO`, `PESONETO`, `PESOBRUTO`, `IDUSUARIO`) VALUES (?,?,?,?,?,?,?,?); ', [codigo,nombre,descripcion, tipounidad ,minimo,pesoneto,pesobruto,req.user.idUSUARIO]);
+    await pool.query('INSERT INTO `inventario`.`historica` (`TRANSACCION`, `TIPOUNIDAD`, `CANTIDAD`, `NOMBRE`, `CODIGO`, `DESCRIPCION`, `ENTRADAS`, `SALIDAS`, `idPRODUCTO`, `idUSUARIO`) VALUES (?,?,?,?,?,?,?,?,?,?);',['ENTRADA',tipounidad,cantidad,nombre, codigo, descripcion,cantidad,0,result.insertId,req.user.idUSUARIO]);
+   
+    req.flash('success', 'Producto Guardado');
+    res.redirect('/producto_list');
+
 });
+
 module.exports = router;
